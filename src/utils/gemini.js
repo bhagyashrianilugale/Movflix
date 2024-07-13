e29964815d637baeec1ef053f80761043746c7f2
bhagyashriugale <bhagyashriugale6970@gmail.com>
2024-07-13 16:08:32 +0530

    logo url changed

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-const GEMINI_KEY = process.env.REACCT_APP_GEMINI_KEY;
+const GEMINI_KEY = process.env.REACT_APP_GEMINI_KEY;
 
 // Gemini API_key set up
 const genAI = new GoogleGenerativeAI(GEMINI_KEY);
